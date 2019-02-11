65df463dfcea6f0ff09790331de2abc93653cc6c
jmorrow2 <36283240+jmorrow2@users.noreply.github.com>
2019-02-11 15:03:43 -0500

    Update pre.c

--- pre.c
+++ pre.c
@@ -2,7 +2,60 @@
 #include<stdlib.h>
 #include<string.h>
 
+char _min_field[50], _max_field[50], _mean_field[50],
+     _record_field[50], _record_value[50];
+int _num_of_fields, _num_of_records; 
+
 int main(int argc, char* argv[]){
+	
+int i=1;
+
+while(i<argc-1){
+    if(strcmp(argv[i], "-f")==0){
+        // displayNumOfFields();
+    }else
+    if(strcmp(argv[i], "-r")==0){
+        // displayNumOfRecords();
+    }else
+    if(strcmp(argv[i], "-h")==0){
+        // headerNotRecord();
+    }else
+    if(strcmp(argv[i], "-max")==0){
+        if((strcmp(argv[i+1],"Ref_Date")!=0) && (strcmp(argv[i+1],"Value")!=0)){
+            printf("%s", "MUST ENTER NUMERIC FIELD");
+            return 1;
+        }
+        strcpy(_max_field, argv[i+1]);
+        i++;
+    }else
+    if(strcmp(argv[i], "-min")==0){
+        if((strcmp(argv[i+1],"Ref_Date")!=0) && (strcmp(argv[i+1],"Value")!=0)){
+            printf("%s", "MUST ENTER NUMERIC FIELD");
+            return 1;
+        }
+        strcpy(_min_field, argv[i+1]);
+        i++;
+    }else
+    if(strcmp(argv[i], "-mean")==0){
+        if((strcmp(argv[i+1],"Ref_Date")!=0) && (strcmp(argv[i+1],"Value")!=0)){
+            printf("%s", "MUST ENTER NUMERIC FIELD");
+            return 1;
+        }
+        strcpy(_mean_field, argv[i+1]);
+        i++;
+    }else
+    if(strcmp(argv[i], "-records")==0){
+        if((strcmp(argv[i+1],"Ref_Date")!=0) && (strcmp(argv[i+1],"Value")!=0)){
+            printf("%s", "MUST ENTER NUMERIC FIELD");
+            return 1;
+        }
+        strcpy(_record_field, argv[i+1]);
+        strcpy(_record_value, argv[i+2]);
+        i++;
+        i++;
+    }
+    i++;
+}
 
   char* fileName = argv[argc -1];
   if(fileName == NULL){
